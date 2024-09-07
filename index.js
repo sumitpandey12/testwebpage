e7da661f08678b3f6017498961750d4b63a022c3
Sumit Pandey <heerjnv@gmail.com>
2024-09-07 17:33:06 +0530

    Donw

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 document.getElementById("btn").addEventListener("click", () => {
-  window.location.href = `intent:abc#Intent;package=com.sumit.player;end`;
+  window.location.href = `intent:abc#Intent;package=com.sumit.player;action=android.intent.action.VIEW;type=video/*;end`;
 });

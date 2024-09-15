65333059ba8885e77eab7ba8d64c1ce01847da04
Sumit Pandey <72408099+sumitpandey12@users.noreply.github.com>
2024-09-15 23:27:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.getElementById("btn").addEventListener("click", () => {
-  const videoURL= "http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8";
+  const videoURL= "https://f2l-dl-1b2e058a42f6.herokuapp.com/171767/%40TamilMob_LinkZz++-+Visfot+%282024%29+WEB-DL+-+1080p+-+AVC+-+%28AA.mkv?hash=AgAD7R";
   window.location.href = `intent:${videoURL}#Intent;package=com.sumit.player;action=android.intent.action.VIEW;type=video/*;end`;
 });

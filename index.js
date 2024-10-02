ef4f464c187fbf848ec6e5383b1090a227529cc7
Sumit Pandey <72408099+sumitpandey12@users.noreply.github.com>
2024-10-02 23:58:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-// document.getElementById("btn").addEventListener("click", () => {
-//   const videoURL= "http://tnplayer4993-a43329ca83d5.herokuapp.com/39694/%2540TamilMob_LinkZz%2B%2B-%2BVisfot%2B%25282024%2529%2BWEB-DL%2B-%2B1080p%2B-%2BAVC%2B-%2B%2528AA.mkv?hash=abc";
-//    window.location.href = `intent:${videoURL}#Intent;package=com.qp.jplayer;action=android.intent.action.VIEW;type=video/*;end`;
-// });
-
 document.getElementById("btn").addEventListener("click", () => {
-  const videoURL = "http://your-video-url.com/video.mp4"; // Update with actual URL
-
-  const intent = new Intent("android.intent.action.VIEW", Uri.parse(videoURL));
-  if (getPackageManager().resolveActivity(intent, 0) !== null) {
-    startActivity(intent); // Launch the app if available
-  } else {
-    // Handle the case where no app is found
-    console.error("No app found to handle video");
-    // Optionally: Inform the user and offer alternatives
-  }
+  const videoURL= "http://tnplayer4993-a43329ca83d5.herokuapp.com/39694/%2540TamilMob_LinkZz%2B%2B-%2BVisfot%2B%25282024%2529%2BWEB-DL%2B-%2B1080p%2B-%2BAVC%2B-%2B%2528AA.mkv?hash=abc";
+   window.location.href = `intent:${videoURL}#Intent;package=com.qp.jplayer;action=android.intent.action.VIEW;end`;
 });

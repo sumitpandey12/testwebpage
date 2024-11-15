f2e85e0cfba6f7849199d2de6c158586101713ff
Sumit Pandey <72408099+sumitpandey12@users.noreply.github.com>
2024-11-15 20:16:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.getElementById("btn").addEventListener("click", () => {
-  const videoURL= "http://tnplayer4993-a43329ca83d5.herokuapp.com/39694/%2540TamilMob_LinkZz%2B%2B-%2BVisfot%2B%25282024%2529%2BWEB-DL%2B-%2B1080p%2B-%2BAVC%2B-%2B%2528AA.mkv?hash=AgAD7R";
+  const videoURL= "https://tnplayer.com/lcFLt";
   window.location.href = `intent:${videoURL}#Intent;package=com.qp.jplayer;action=android.intent.action.VIEW;type=video/*;end`;
 });

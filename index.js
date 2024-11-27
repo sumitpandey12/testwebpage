e1328773402415a3aa3ebb7e416b028dd370ea72
Sumit Pandey <72408099+sumitpandey12@users.noreply.github.com>
2024-11-27 23:08:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.getElementById("btn").addEventListener("click", () => {
-  const videoURL= "https://tnplayer.com/lcFLt";
+  const videoURL= "https://tnplayer.com/NOVyl";
   window.location.href = `intent:${videoURL}#Intent;package=com.qp.jplayer;action=android.intent.action.VIEW;type=video/*;end`;
 });

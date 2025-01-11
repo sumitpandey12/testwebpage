90bfe66c880842badac67ecdeaf747e136029509
Sumit Pandey <72408099+sumitpandey12@users.noreply.github.com>
2025-01-11 19:11:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ const vURL='https://tnplayer4993-a43329ca83d5.herokuapp.com/59434/Valliyin_Velan
 
 document.getElementById("btn").addEventListener("click", () => {
   const videoURL= "https://tnplayer.com/EAXFm";
-   // window.location.href = `intent:https://tnplayer.com/EAXFm#Intent;package=com.qp.jplayer;action=android.intent.action.VIEW;end`;
-	 window.location.href = `intent:${videoURL}#Intent;package=com.qp.jplayer;action=android.intent.action.VIEW;type=video/*;end`, '_blank';
-});
-
-document.getElementById("btn2").addEventListener("click", () => {
-  const videoURL= "https://tnplayer4993-a43329ca83d5.herokuapp.com/59434/Valliyin_Velan_S01E20_Valliyin_Velan_September_27_2024_360p_ZEE5.mkv?hash=AgAD4h?api=122.161.69.8";
-   window.location.href = `intent:${videoURL}#Intent;package=com.qp.jplayer;action=android.intent.action.VIEW;end`;
+   window.location.href = `intent:${videoURL}#Intent;package=com.qp.jplayer;action=android.intent.action.VIEW;type=video/*;end`, '_blank';
 });
 
 document.getElementById("btn1").addEventListener("click", () => {
   const videoURL= "http://tnplayer4993-a43329ca83d5.herokuapp.com/39694/%2540TamilMob_LinkZz%2B%2B-%2BVisfot%2B%25282024%2529%2BWEB-DL%2B-%2B1080p%2B-%2BAVC%2B-%2B%2528AA.mkv";
    window.location.href = `intent:${vURL}#Intent;package=com.qp.jplayer;action=android.intent.action.VIEW;type=video/*;end`, '_blank';
+});
+
+document.getElementById("btn2").addEventListener("click", () => {
+  const videoURL= `${vURL}?api=af9359286a557911b6989d43009b37bb`;
+   window.location.href = `intent:${videoURL}#Intent;package=com.qp.jplayer;action=android.intent.action.VIEW;type=video/*;end`, '_blank';
 });
